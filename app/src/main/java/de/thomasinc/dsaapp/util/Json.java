7330c060df8051e415ca1b564296b0da3e006e1c
thomas <t.grieb@campus.lmu.de>
2021-02-18 16:45:56 +0100

    added various functions for reading data from json files (unfinished readcharfromjson)

--- app/src/main/java/de/thomasinc/dsaapp/util/Json.java
+++ app/src/main/java/de/thomasinc/dsaapp/util/Json.java
@@ -1,16 +1,18 @@
 package de.thomasinc.dsaapp.util;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Writer;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -22,12 +24,18 @@ import de.thomasinc.dsaapp.data.character.Character;
  */
 
 public class Json {
+
+    private static final String FILE_ENDING_JSON = ".json";
+    private static final String PROFILE_MAP = "profilemap" + FILE_ENDING_JSON;
+
+
     /**
      * Reads the skills from the json file saved in the apps' assets folder
+     *
      * @param context application context needed for filepath
      * @return {@link String} of skills
      */
-    public static String readSkillsJson(Context context){
+    public static String readSkillsJson(Context context) {
         String json = null;
         try {
             InputStream file = context.getAssets().open("skills.json");
@@ -43,15 +51,82 @@ public class Json {
     }
 
     /**
+     * Checks whether the profile map file exists.
+     * If it doesn't exist, it's created here.
+     * The function is intended for calling right after starting the app
+     * (currently in the constructor of {@link de.thomasinc.dsaapp.ui.main.MainPresenter}.
+     *
+     * @param context applications context
+     */
+    public static void checkProfileMapFile(Context context) {
+        File profileFile = new File(context.getFilesDir(), PROFILE_MAP);
+        if (!profileFile.exists()) {
+            try {
+                profileFile.createNewFile();
+                Log.i("Json","Created profilemap file.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Retrieves the contents of the profile map file
+     *
+     * @param context application context
+     * @return profile map
+     */
+    public static JSONObject readProfileMapFile(Context context){
+        JSONObject jobjMap = new JSONObject();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    context.openFileInput(PROFILE_MAP), StandardCharsets.UTF_8));
+            String json = reader.readLine();
+            if (json != null) {
+                try {
+                    jobjMap = new JSONObject(json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jobjMap;
+    }
+
+    /**
+     * Retrieves a specific characters filepath using readProfileMapFile
+     *
+     * @param context application context
+     * @param name characters name
+     * @return the corresponding filename
+     */
+    public static String getCharFilepath(Context context, String name){
+        String filename = "";
+        try {
+            JSONObject jobj = readProfileMapFile(context);
+            filename = jobj.getString(name);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        return filename;
+    }
+
+    /**
      * Reads existing charfile (json) from the  appropriate file folder and creates corresponding
-     *  {@link Character} object.
+     * {@link Character} object.
+     *
      * @param context application context needed for filepath
      * @return {@link Character} object with the saved attributes
      */
-    public static Character readCharFromJson(Context context) {
+    public static Character readCharFromJson(Context context, String name) {
         ArrayList<Integer> l = new ArrayList<>();
         String json = null;
         try {
+            //BufferedReader reader = new BufferedReader(new InputStreamReader(
+            //        context.openFileInput(), StandardCharsets.UTF_8));
+
             InputStream file = context.openFileInput("myCharacter.json"); //const-class?
             int size = file.available();
             byte[] buffer = new byte[size];
@@ -61,7 +136,9 @@ public class Json {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
         try {
+            assert json != null;
             JSONObject obj = new JSONObject(json);
             Iterator<String> it = obj.keys();
             while (it.hasNext()) {
@@ -70,6 +147,7 @@ public class Json {
         } catch (JSONException ex) {
             ex.printStackTrace();
         }
+
         return new Character.CharBuilder("placeholder")
                 .mu(l.get(0))
                 .kl(l.get(1))
@@ -83,36 +161,60 @@ public class Json {
     }
 
     /**
-     * Writes a Character object to a json file, derives the filename from the charactername
+     * Writes a Character object to a json file, derives the filename from the charactername.
+     * Also adds name and full filename to a separate json for name-to-filename mapping.
+     *
      * @param context application context
-     * @param c needs to include at least the characters name
+     * @param c       needs to include at least the characters name
      */
     public static void writeCharToJson(Context context, Character c) {
-        JSONObject jobj = new JSONObject();
+        String name = c.getName();
+        String filename = Util.normalizeString(name) + FILE_ENDING_JSON;
+
+        //TODO create seperate attribute-dictionary in character json file
+        JSONObject jobjChar = new JSONObject();
         try {
-            jobj.put("Name",c.getName());
-            if (c.getMu()!=0) {
-                jobj.put("MU", c.getMu());
-                jobj.put("KL", c.getKl());
-                jobj.put("IN", c.getIn());
-                jobj.put("CH", c.getCh());
-                jobj.put("FF", c.getFf());
-                jobj.put("GE", c.getGe());
-                jobj.put("KO", c.getKo());
-                jobj.put("KK", c.getKk());
+            jobjChar.put("Name", name);
+            if (c.getMu() != 0) {
+                jobjChar.put("MU", c.getMu());
+                jobjChar.put("KL", c.getKl());
+                jobjChar.put("IN", c.getIn());
+                jobjChar.put("CH", c.getCh());
+                jobjChar.put("FF", c.getFf());
+                jobjChar.put("GE", c.getGe());
+                jobjChar.put("KO", c.getKo());
+                jobjChar.put("KK", c.getKk());
             }
-        }
-        catch (JSONException er){
+        } catch (JSONException er) {
             er.printStackTrace();
         }
+
+        File charFile = new File(context.getFilesDir(), filename);
         try {
-            Writer output = null;
-            File file = new File(context.getFilesDir(), Util.normalizeString(c.getName()));
-            output = new BufferedWriter(new FileWriter(file));
-            output.write(jobj.toString());
-            output.close();
-        } catch (IOException er){
+            BufferedWriter writer = new BufferedWriter(new FileWriter(charFile));
+            System.out.println(charFile);
+            writer.write(jobjChar.toString());
+            writer.close();
+        } catch (IOException er) {
             er.printStackTrace();
         }
+
+        JSONObject jobjMap = readProfileMapFile(context);
+        try {
+            jobjMap.put(name, filename);
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+
+        File profileFile = new File(context.getFilesDir(), PROFILE_MAP);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(profileFile));
+            writer.write(jobjMap.toString());
+            writer.close();
+            System.out.println("Profilemap: " + jobjMap.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
