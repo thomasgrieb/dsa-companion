26025c8ba5b8c9dd8de5352c7ce996cbdaf87da8
thomas <t.grieb@campus.lmu.de>
2021-02-22 19:43:59 +0100

    adde constant for intent extra (skill category)

--- app/src/main/java/de/thomasinc/dsaapp/util/ConstantsGlobal.java
+++ app/src/main/java/de/thomasinc/dsaapp/util/ConstantsGlobal.java
@@ -4,8 +4,12 @@ package de.thomasinc.dsaapp.util;
  * Includes important global constants that are referenced from several different classes
  */
 public class ConstantsGlobal {
+    //Shared Preferences
     public static final String PREFERENCES_FILE = "Characters_Prefs";
     public static final String PREFERENCES_CURRENT_CHAR_KEY = "current";
     public static final String PREFERENCES_CHARACTER_LIST_KEY = "characters";
+
+    //Intent extras
+    public static final String EXTRA_SKILL_CAT = "cat";
 
 }
